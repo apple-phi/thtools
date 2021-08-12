d3c4301a07266799b228f852ef073b9bf44d389c
lkn849 <lkn849@gmail.com>
2021-08-13 00:33:33 +0200

    :test_tube: iGEM: update cloud build

--- registry/deploy.sh
+++ registry/deploy.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
+
 NAME="thtools-1"
-ZONE=europe-west1-c
+ZONE=europe-west2-c
 
 gcloud compute instances create $NAME \
     --image-family=ubuntu-minimal-2004-lts  \
     --image-project=ubuntu-os-cloud \
     --machine-type=n2d-highcpu-8 \
-    --scopes userinfo-email,cloud-platform \
-    --metadata-from-file startup-script=startup-script.sh \
+    --metadata-from-file startup-script=startup.sh \
     --zone $ZONE \
-    --tags http-server
+    --scopes cloud-platform
 
 gcloud compute instances get-serial-port-output $NAME --zone $ZONE
