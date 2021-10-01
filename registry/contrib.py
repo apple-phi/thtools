db4b0cb4ddaec9a792a04be967d4851264390372
lkn849 <lkn849@gmail.com>
2021-10-01 13:28:29 +0100

    :test_tube: CONTRIB: reserve CPU core

--- registry/contrib.py
+++ registry/contrib.py
@@ -180,10 +180,16 @@ class Contribution:
                     with tqdm.tqdm(
                         total=len(CELSIUS_RANGE) + 1, desc=toehold_name, leave=None
                     ) as switch_bar:
-                        thtest.run(max_size=3 + len(antis) + EXTRA_SIZE)
+                        thtest.run(
+                            max_size=3 + len(antis) + EXTRA_SIZE,
+                            n_nodes=tt.CPU_COUNT - 1,
+                        )
                         switch_bar.update()
                         crt = tt.CelsiusRangeTest(thtest, CELSIUS_RANGE)
-                        for _ in crt.generate(max_size=3 + len(antis) + EXTRA_SIZE):
+                        for _ in crt.generate(
+                            max_size=3 + len(antis) + EXTRA_SIZE,
+                            n_nodes=tt.CPU_COUNT - 1,
+                        ):
                             switch_bar.update()
                     self.save(mirna, toehold_name, thtest.result, crt.result, team_bar)
                     del thtest
